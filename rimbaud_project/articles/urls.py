c3c14fab116f2902fbf0114128ae7e46fbfa405f
Audrey <audrey.cornaire@gmail.com>
2021-05-07 16:11:44 +0200

    Search page and button

--- rimbaud_project/articles/urls.py
+++ rimbaud_project/articles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='all_articles'),
     path('article/<int:id>/', views.article, name='article_view'),
-    path('', views.index, name='all_articles'),    
+    path('search/<str:words>', views.search, name='article_search')
 ]
--- rimbaud_project/articles/views.py
+++ rimbaud_project/articles/views.py
@@ -11,6 +11,9 @@ def index(request):
     return render(request, template_name='articles_list.html', context={'articles':articles})
 
 def article(request, id):
-    HttpResponse('Hello')
     article = Article.objects.get(id=id)
     return render(request, template_name='article_view.html', context={'article':article})
+
+def search(request, words):
+    articles = Article.objects.filter(limitdate__range=[datetime.datetime.now(), '3021-12-31'], title__icontains=words)
+    return render(request, template_name='articles_search.html', context={'articles':articles, 'words':words})

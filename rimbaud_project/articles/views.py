a42a5833dd6964139cbdfc4f0327c68d169c3eae
Audrey <audrey.cornaire@gmail.com>
2021-05-07 14:41:16 +0200

    Update filter on list page

--- rimbaud_project/articles/views.py
+++ rimbaud_project/articles/views.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.urls.conf import include
 from articles.models import Article
-#from django.utils import timezone
+import datetime
 
 def index(request):
-    articles = Article.objects.all().order_by('release')
+    articles = Article.objects.filter(limitdate__range=[datetime.datetime.now(), '3021-12-31']).order_by('release')
     return render(request, template_name='articles_list.html', context={'articles':articles})
 
 def article(request, id):

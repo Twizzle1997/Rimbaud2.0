f32470f297a5aa3baf0cc680b3830be57ec484bf
ThibaultDataDev <67363761+ThibaultDataDev@users.noreply.github.com>
2021-05-07 11:54:42 +0200

    article_insane

--- rimbaud_project/articles/admin.py
+++ rimbaud_project/articles/admin.py
@@ -4,4 +4,4 @@ from .models import Article
 
 @admin.register(Article)
 class AuthorAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'body')
+    fields = ('title', 'description', 'body', 'limitdate', 'adminonly', 'created_date', 'release')
